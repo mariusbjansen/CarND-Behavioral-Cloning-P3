03dbe30defc94c05bfc1b58506cc10634628392a
Marius Jansen <mariusb.jansen@googlemail.com>
2017-10-29 17:47:28 +0100

    updated and video upload

--- drive.py
+++ drive.py
@@ -23,11 +23,12 @@ prev_image_array = None
 
 
 class SimplePIController:
-    def __init__(self, Kp, Ki):
+    def __init__(self, Kp, Ki, Kd):
         self.Kp = Kp
         self.Ki = Ki
         self.set_point = 0.
         self.error = 0.
+        self.error_k1 = 0.
         self.integral = 0.
 
     def set_desired(self, desired):
@@ -40,11 +41,15 @@ class SimplePIController:
         # integral error
         self.integral += self.error
 
+	    # differential error
+        de = self.error-self.error_k1	
+        
+        self.error_k1 = self.error
         return self.Kp * self.error + self.Ki * self.integral
 
 
-controller = SimplePIController(0.1, 0.002)
-set_speed = 12
+controller = SimplePIController(0.3, 0.002, 0.00)
+set_speed = 15
 controller.set_desired(set_speed)
 
 
